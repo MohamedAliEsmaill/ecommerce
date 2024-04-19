aa7c583f755f73c6bc25043a8a083a2058077775
Mahmoud Osama Dabbous <109554499+MahmoudDabbous@users.noreply.github.com>
2024-04-19 17:42:23 +0200

    refactor: remove orderId from order model

--- server/models/Order.mjs
+++ server/models/Order.mjs
@@ -1,11 +1,6 @@
 import mongoose from 'mongoose';
 
 const orderSchema = new mongoose.Schema({
-    orderId: {
-        type: Number,
-        required: true,
-        unique: true,
-    },
     date: { type: Date, default: Date.now },
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     products: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }],
--- server/models/User.mjs
+++ server/models/User.mjs
@@ -1,21 +1,22 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    email: { type: String, required: true, unique: true },
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    name: { type: String },
-    phone: { type: String, required: true },
-    address: { type: String },
-    isAdmin: { type: Boolean, required: true, default: false },
-    // TODO - todo note orders and carts logic
-    orders: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Order' }],
-    carts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }],
-    image: { type: String, default: 'https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/default-profile-picture-grey-male-icon.png' },
-    gender: { type: String, enum: ['male', 'female'] }
+  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  name: { type: String },
+  phone: { type: String, required: true },
+  address: { type: String },
+  isAdmin: { type: Boolean, required: true, default: false },
+  orders: [{ type: mongoose.Schema.Types.ObjectId, ref: "Order" }],
+  carts: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
+  image: {
+    type: String,
+    default:
+      "https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/default-profile-picture-grey-male-icon.png",
+  },
+  gender: { type: String, enum: ["male", "female"] },
 });
 
-
-
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 export default User;
