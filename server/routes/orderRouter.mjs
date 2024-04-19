8c9ddae92c08e57adcc5f30c1dfcc76e7949b9f6
Mahmoud Osama Dabbous <109554499+MahmoudDabbous@users.noreply.github.com>
2024-04-19 22:41:41 +0200

    feat: implement orders controller and add order routes

--- server/routes/orderRouter.mjs
+++ server/routes/orderRouter.mjs
@@ -1,10 +1,13 @@
 import express from "express";
+import orderController from "../controllers/orderController.mjs";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello Orders!");
-});
-
+router.get("/", orderController.getAllOrders);
+router.get("/:id", orderController.getOrderById);
+router.post("/", orderController.createOrder);
+router.put("/:id/accept", orderController.updateOrderToAccepted);
+router.put("/:id/reject", orderController.updateOrderToRejected);
+router.delete("/:id/cancel", orderController.cancelOrder);
 
 export default router;
