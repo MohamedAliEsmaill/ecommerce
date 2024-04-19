536181b9faf868333f54029d4b8b39791163aa4e
Marwan Mohamed <marawanmohamed9876@gmail.com>
2024-04-20 01:25:46 +0200

    feat: :truck: added routes and controller functions

--- server/routes/productRouter.mjs
+++ server/routes/productRouter.mjs
@@ -1,10 +1,13 @@
 import express from "express";
+import { getAllProducts, createProduct, getProductId, updateProduct, deleteProduct } from '../controllers/productController.mjs';
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello Product!");
-});
+router.get("/", getAllProducts);
+router.get("/:id", getProductId);
+router.post("/", createProduct);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
 
 
 export default router;
