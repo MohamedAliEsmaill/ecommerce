aaa43ce838616dbe6019d40c25ce4ce91e874ba7
mahmudhmh <mahmoudh.buss@gmail.com>
2024-04-20 04:36:00 +0200

    feat: Add forgetPassword, resetPassword, and updatePassword functions
    * Added new functions for handling password management.
    * Implemented restricted routes and role-based access control.
    
    fix:
    * Updated User Model to use role as an enum for better data integrity.
    
    feat:
    * Integrated nodemailer and mailtrap for email functionalities related to password management.

--- server/app.mjs
+++ server/app.mjs
@@ -2,13 +2,20 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes.mjs";
+import appError from "./utils/appError.mjs";
 import { faker } from "@faker-js/faker";
 import Product from "./models/Product.mjs";
 import FeaturedProducts from "./models/FeaturedProducts.mjs";
 import User from "./models/User.mjs";
 import Order from "./models/Order.mjs";
 
-dotenv.config();
+process.on("uncaughtException", (err) => {
+  console.log("uncaught exception ... shutting down");
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
+dotenv.config({ path: "./config.env" });
 const app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -31,7 +38,7 @@ process.on("unhandledRejection", (error) => {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(authRoutes);
+app.use("/api/users", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -39,4 +46,18 @@ app.get("/", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Listening on 127.0.0.1:${PORT}`);
+});
+
+app.all("*", (req, res, next) => {
+  next(new appError(`Can't find ${req.originalUrl} on this server`));
+});
+
+// HANDLE UNHANDLED REJECTED PROMISES
+process.on("unhandledRejection", (err) => {
+  console.log("unhandled rejection ... shutting down");
+  console.log(err.name, err.message);
+  // 0 : success , 1 : uncaught exception
+  server.close(() => {
+    process.exit(1);
+  });
 });
--- server/models/User.mjs
+++ server/models/User.mjs
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import crypto from "crypto";
+import validator from "validator";
 
 const userSchema = new mongoose.Schema({
   fullName: {
     type: String,
-    required: [true, "fullname not provided "],
+    required: [true, "Full name not provided"],
   },
   email: {
     type: String,
@@ -16,12 +19,38 @@ const userSchema = new mongoose.Schema({
       message: "{VALUE} is not a valid email!",
     },
   },
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  name: { type: String },
-  phone: { type: String, required: true },
-  address: { type: String },
-  isAdmin: { type: Boolean, required: true, default: false },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "user must have a password"],
+    minlength: 8,
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, "please confirm your password"],
+    // validate works on save() , create() - doesn't work on findByIdAndUpdate
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: "passwords match match",
+    },
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  address: String,
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+  },
   orders: [{ type: mongoose.Schema.Types.ObjectId, ref: "Order" }],
   carts: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
   image: {
@@ -29,9 +58,61 @@ const userSchema = new mongoose.Schema({
     default:
       "https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/default-profile-picture-grey-male-icon.png",
   },
-  gender: { type: String, enum: ["male", "female"] },
-  createdAt: { type: Date, default: Date.now },
+  gender: {
+    type: String,
+    enum: ["male", "female"],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  passwordChangedAt: Date,
+  passwordResetToken: String,
+  passwordResetExpires: Date,
 });
 
+// Hash password and set passwordConfirm before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = this.password;
+  next();
+});
+
+// Create password reset token and set expiry
+userSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString("hex");
+  this.passwordResetToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000; // Token expires in 10 minutes
+  return resetToken;
+};
+
+userSchema.pre(/^find/, function (next) {
+  this.find({ active: { $ne: false } });
+  next();
+});
+
+// Check if provided password matches user's password
+userSchema.methods.correctPassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+// Check if user changed password after the token was issued
+userSchema.methods.changePasswordAfter = function (JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    const changedTimestamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10
+    );
+    return JWTTimestamp < changedTimestamp;
+  }
+  return false;
+};
+
 const User = mongoose.model("User", userSchema);
+
 export default User;
