671401633bb10a499d058d11f3e5c8a8b4ff2189
Mohamed Hossny <mhossny2@outlook.com>
2024-04-20 23:04:03 +0200

    Refactor(userRouter): use protect middleware for /cart routes

--- server/routes/userRouter.mjs
+++ server/routes/userRouter.mjs
@@ -1,14 +1,14 @@
 import express from "express";
-import { addCart , getCart } from "../controllers/userController.mjs";
-import { verifyToken } from "../middleware/authJWT.mjs";
+import { addCart, getCart } from "../controllers/userController.mjs";
+import { protect } from "../controllers/authController.mjs";
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.send("Hello User!");
 });
 
-router.get("/cart", verifyToken, getCart)
-router.post("/cart", verifyToken, addCart);
+router.get("/cart", protect, getCart);
+router.post("/cart", protect, addCart);
 // router.put("/cart", (req, res) => {
 //   res.send("Hello User!");
 // });
