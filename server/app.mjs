dd36d61d87cf8323bffbeaac88512fcfafa14b9d
Mahmoud Osama Dabbous <109554499+MahmoudDabbous@users.noreply.github.com>
2024-04-21 21:29:52 +0200

    Merge branch 'main' of https://github.com/MohamedAliEsmaill/ecommerce into Backend/users-api (#19)

--- server/app.mjs
+++ server/app.mjs
@@ -1,8 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import authRoutes from "./routes/authRoutes.mjs";
+import authRouter from "./routes/authRouter.mjs";
 import appError from "./utils/appError.mjs";
+import { protect } from "./controllers/authController.mjs";
 import userRouter from "./routes/userRouter.mjs";
 import orderRouter from "./routes/orderRouter.mjs";
 import productRouter from "./routes/productRouter.mjs";
@@ -43,10 +44,10 @@ process.on("unhandledRejection", (error) => {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api/users", authRoutes);
+app.use("/api/users", authRouter);
 app.use("/api/users", userRouter);
 app.use("/api/profile", userRouter);
-app.use("/api/orders", orderRouter);
+app.use("/api/orders", protect, orderRouter);
 app.use("/api/products", productRouter);
 app.use("/api/featuredproducts", featuredProductsRouter);
 
---
+++ server/controllers/ordersController.mjs
@@ -0,0 +1,127 @@
+import Order from "../models/Order.mjs";
+
+export const getOrderById = async (req, res) => {
+  try {
+    const order = await Order.findById(req.params.id).populate("user");
+    if (order) {
+      res.json(order);
+    } else {
+      res.status(404);
+      throw new Error("Order not found");
+    }
+  } catch (error) {
+    res.status(500);
+    throw new Error("Error while fetching order");
+  }
+};
+
+export const getAllOrders = async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const startIndex = (page - 1) * limit;
+
+    const totalOrders = await Order.countDocuments();
+    const totalPages = Math.ceil(totalOrders / limit);
+
+    const orders = await Order.find({})
+      .populate("user")
+      .skip(startIndex)
+      .limit(limit);
+
+    const pagination = {
+      currentPage: page,
+      totalPages: totalPages,
+      totalOrders: totalOrders,
+    };
+
+    res.json({ orders, pagination });
+  } catch (error) {
+    res.status(500);
+    throw new Error("Error while fetching orders");
+  }
+};
+
+export const addOrder = async (req, res) => {
+  const { products, totalPrice } = req.body;
+
+  if (!products || products.length === 0) {
+    res
+      .status(400)
+      .json({
+        error: "No products in the order, please add products to the order",
+      });
+    return;
+  }
+
+  try {
+    const createdOrder = await Order.create({
+      products,
+      totalPrice,
+      user: req.user._id,
+      date: Date.now(),
+    });
+
+    req.user.cart = [];
+    await req.user.save();
+
+    res.status(201).json(createdOrder);
+  } catch (error) {
+    console.error("Error while creating the order:", error);
+    res.status(500).json({ error: "Error while creating the order" });
+  }
+};
+
+export const updateOrderToAccepted = async (req, res) => {
+  try {
+    const order = await Order.findById(req.params.id);
+
+    if (order) {
+      order.status = "accepted";
+
+      const updatedOrder = await order.save();
+      res.json(updatedOrder);
+    } else {
+      res.status(404);
+      throw new Error("Order not found");
+    }
+  } catch (error) {
+    res.status(500);
+    throw new Error("Error while updating order to accepted");
+  }
+};
+
+export const updateOrderToRejected = async (req, res) => {
+  try {
+    const order = await Order.findById(req.params.id);
+
+    if (order) {
+      order.status = "rejected";
+
+      const updatedOrder = await order.save();
+      res.json(updatedOrder);
+    } else {
+      res.status(404);
+      throw new Error("Order not found");
+    }
+  } catch (error) {
+    res.status(500);
+    throw new Error("Error while updating order to rejected");
+  }
+};
+
+export const deleteOrder = async (req, res) => {
+  try {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+      await order.deleteOne();
+      res.json({ message: "Order deleted" });
+    } else {
+      res.status(404);
+      throw new Error("Order not found");
+    }
+  } catch (error) {
+    res.status(500);
+    throw new Error("Error while deleting order");
+  }
+};
