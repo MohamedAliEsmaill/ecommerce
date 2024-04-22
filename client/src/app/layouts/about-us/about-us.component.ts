1d176c3e5493a6480e8e3536a1dc41e34acfbc73
Marwan Mohamed <marawanmohamed9876@gmail.com>
2024-04-22 21:42:12 +0200

    feat: :zap: created all the shared components , contact-us page and about-us page

---
+++ client/src/app/layouts/about-us/about-us.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-us',
+  standalone: true,
+  imports: [],
+  templateUrl: './about-us.component.html',
+  styles: ``
+})
+export class AboutUsComponent {
+
+}
---
+++ client/src/app/layouts/contact-us/contact-us.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-us',
+  standalone: true,
+  imports: [],
+  templateUrl: './contact-us.component.html',
+  styles: ``
+})
+export class ContactUsComponent {
+
+}
