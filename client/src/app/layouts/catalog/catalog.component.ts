82ba463b9bdcced9a787862967a9aa9918bf9ede
Marwan Mohamed <marawanmohamed9876@gmail.com>
2024-04-22 23:03:36 +0200

    feat: :art: created product list layout

---
+++ client/src/app/layouts/catalog/catalog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catalog',
+  standalone: true,
+  imports: [],
+  templateUrl: './catalog.component.html',
+  styles: ``
+})
+export class CatalogComponent {
+
+}
