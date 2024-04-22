60cd2ab479107e809a18f5d7474b7827d7a6cb45
Marwan Mohamed <marawanmohamed9876@gmail.com>
2024-04-22 23:03:59 +0200

    refactor: :truck: added the catalog route

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { ContactUsComponent } from './layouts/contact-us/contact-us.component';
 import { AboutUsComponent } from './layouts/about-us/about-us.component';
 import { LoginComponent } from './layouts/login/login.component';
 import { SignupComponent } from './layouts/signup/signup.component';
+import { CatalogComponent } from './layouts/catalog/catalog.component';
 
 export const routes: Routes = [
   { path: 'contact-us', component: ContactUsComponent },
   { path: 'about-us', component: AboutUsComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'signup', component: SignupComponent }
+  { path: 'signup', component: SignupComponent },
+  { path: 'catalog', component: CatalogComponent }
 ];
