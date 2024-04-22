7bd1a0157caa708ec14b6e6ef7e8bb294b860cc2
mahmudhmh <mahmoudh.buss@gmail.com>
2024-04-22 20:46:19 +0200

    feat(Auth): UI skeleton for Auth flow

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+export const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
+  },
+];
