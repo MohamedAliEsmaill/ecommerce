e1f4e2cc71cb22d1103696f3781546f7fcf3e59c
Marwan Mohamed <marawanmohamed9876@gmail.com>
2024-04-22 22:12:55 +0200

    refactor: :truck: add routes for singin and singup components

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { ContactUsComponent } from './layouts/contact-us/contact-us.component';
 import { AboutUsComponent } from './layouts/about-us/about-us.component';
+import { LoginComponent } from './layouts/login/login.component';
+import { SignupComponent } from './layouts/signup/signup.component';
 
 export const routes: Routes = [
   { path: 'contact-us', component: ContactUsComponent },
-  { path: 'about-us', component: AboutUsComponent }
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent }
 ];
