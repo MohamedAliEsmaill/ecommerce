bc73c9cc091c3f87db8bb5a99fdecbece0507e0e
Ahmed Galal <ahmedgalalmohamed912@gmail.com>
2024-04-23 00:56:04 +0200

    feat&style(cart): add functions to handle the add or remove
    1.add product to your cart
    2.delete product from cart
    3.get products from cart

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { ContactUsComponent } from './layouts/contact-us/contact-us.component';
 import { AboutUsComponent } from './layouts/about-us/about-us.component';
-
+import { CartComponent } from './components/cart/cart.component';
 export const routes: Routes = [
   { path: 'contact-us', component: ContactUsComponent },
-  { path: 'about-us', component: AboutUsComponent }
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'cart', component: CartComponent },
 ];
