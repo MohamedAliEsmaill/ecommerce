26cd9d54bcbf279688fb0e83bd2987296517c629
Marwan Mohamed <marawanmohamed9876@gmail.com>
2024-04-23 20:39:53 +0200

    feat: :art: created product card component

---
+++ client/src/app/components/product-card/product-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-card.component.html',
+  styles: ``
+})
+export class ProductCardComponent {
+
+}
