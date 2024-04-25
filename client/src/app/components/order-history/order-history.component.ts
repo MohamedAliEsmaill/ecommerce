880021b8cd2d60ebca9a5bf70a27ab000b850b52
Mahmoud Osama Dabbous <109554499+MahmoudDabbous@users.noreply.github.com>
2024-04-25 21:54:59 +0200

    feet: Add order service and order service spec files

--- client/src/app/components/order-history/order-history.component.ts
+++ client/src/app/components/order-history/order-history.component.ts
@@ -1,12 +1,83 @@
+import { OrderService } from './../../services/order/order.service';
 import { Component } from '@angular/core';
+import { Order } from '../../interfaces/order';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-order-history',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './order-history.component.html',
-  styleUrl: './order-history.component.css'
+  styleUrl: './order-history.component.css',
+  providers: [OrderService],
 })
 export class OrderHistoryComponent {
+  orders: Order[] = [
+    {
+      _id: '',
+      user: '',
+      products: [],
+      totalPrice: 0,
+      status: 'pending',
+      date: new Date(),
+      address: {
+        street: '',
+        city: '',
+        zip: '',
+      },
+    },
+  ];
+  pagination: {
+    currentPage: number;
+    totalPages: number;
+    totalOrders: number;
+  } = {
+    currentPage: 1,
+    totalPages: 1,
+    totalOrders: 0,
+  };
+  selectedOrder: any = {};
 
+  ngOnInit(): void {
+    this.order.getOrders(this.pagination.currentPage, 5).subscribe({
+      next: (orders) => {
+        console.log(orders);
+
+        this.orders = orders.orders;
+        this.pagination = orders.pagination;
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
+  }
+
+  getOrders() {
+    this.order.getOrders(this.pagination.currentPage, 5).subscribe({
+      next: (orders) => {
+        this.orders = orders.orders;
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
+  }
+
+  nextPage() {
+    if (this.pagination.currentPage < this.pagination.totalPages) {
+      console.log(`Current page: ${this.pagination.currentPage}`);
+
+      this.pagination.currentPage++;
+      this.getOrders();
+    }
+  }
+
+  prevPage() {
+    if (this.pagination.currentPage > 1) {
+      this.pagination.currentPage--;
+      this.getOrders();
+    }
+  }
+
+  constructor(private order: OrderService) {}
 }
---
+++ client/src/app/services/order/order.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Order } from '../../interfaces/order';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class OrderService {
+  private API_URL = 'http://localhost:3000/api/orders';
+
+  constructor(private http: HttpClient) {}
+
+  createOrder(order: any) {
+    return this.http.post<any>(this.API_URL, order);
+  }
+
+  getOrders(
+    page: number | undefined = undefined,
+    pageSize: number | undefined = undefined
+  ) {
+    const params = new HttpParams()
+      .set('page', page ? page.toString() : '')
+      .set('limit', pageSize ? pageSize.toString() : '');
+
+    return this.http.get<{
+      orders: Order[];
+      pagination: {
+        currentPage: number;
+        totalPages: number;
+        totalOrders: number;
+      };
+    }>(this.API_URL, { params });
+  }
+
+  getOrderById(id: string) {
+    return this.http.get<Order>(`${this.API_URL}/${id}`);
+  }
+}
