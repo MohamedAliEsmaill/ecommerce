e813d586b60c8d51f19808d3e4eb0edc33cda50b
Mahmoud Osama Dabbous <109554499+MahmoudDabbous@users.noreply.github.com>
2024-04-25 23:21:46 +0200

    Refactor: Update getAllOrders function in ordersController.mjs to include user filter

--- server/controllers/ordersController.mjs
+++ server/controllers/ordersController.mjs
@@ -20,11 +20,12 @@ export const getAllOrders = async (req, res) => {
     const page = parseInt(req.query.page) || 1;
     const limit = parseInt(req.query.limit) || 10;
     const startIndex = (page - 1) * limit;
+    const user = req.user;
 
-    const totalOrders = await Order.countDocuments();
+    const totalOrders = await Order.countDocuments({ user: user._id });
     const totalPages = Math.ceil(totalOrders / limit);
 
-    const orders = await Order.find({})
+    const orders = await Order.find({ user: user._id })
       .populate("user")
       .skip(startIndex)
       .limit(limit);
