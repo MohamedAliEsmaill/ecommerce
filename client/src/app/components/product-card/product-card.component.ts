ec1380c8420946071c7f80c3657598ddcd3e816a
Marwan Mohamed <marawanmohamed9876@gmail.com>
2024-04-28 01:10:30 +0300

    feat: implemented the wishlist button in catalog layout

--- client/src/app/components/product-card/product-card.component.ts
+++ client/src/app/components/product-card/product-card.component.ts
@@ -2,20 +2,22 @@ import { Component, Input } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { UserServiceService } from '../../services/user/user-service.service';
 import Swal from 'sweetalert2';
+import { LocalStorageService } from '../../services/local-storage.service';
 
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
   imports: [RouterModule],
-  providers: [UserServiceService],
+  providers: [UserServiceService, LocalStorageService],
   templateUrl: './product-card.component.html',
   styles: ``
 })
 export class ProductCardComponent {
   @Input() product: any;
+  wishListBtn = false;
 
-  constructor(private userService: UserServiceService) { }
+  constructor(private userService: UserServiceService, private localStorage: LocalStorageService) { }
 
   addToCart() {
     console.log(this.product._id);
@@ -38,4 +40,20 @@ export class ProductCardComponent {
     })
   }
 
+  addProductToWishList() {
+    let products: any;
+    if (this.localStorage.getItem('wishList')) {
+      products = this.localStorage.getItem('wishList');
+      if (products.some((prod: any) => prod._id === this.product._id)) {
+        products = products.filter((prod: any) => prod._id !== this.product._id);
+      } else {
+        products.push(this.product)
+      }
+      this.localStorage.setItem('wishList', products);
+    } else {
+      products = [];
+      products.push(this.product);
+      this.localStorage.setItem('wishList', products);
+    }
+  }
 }
