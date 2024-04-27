e757ec5a3fcae9f62963cb17c4a37303be6b83da
Marwan Mohamed <marawanmohamed9876@gmail.com>
2024-04-27 22:30:07 +0300

    feat: added toast for addition in cart

--- client/src/app/components/product-card/product-card.component.ts
+++ client/src/app/components/product-card/product-card.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { UserServiceService } from '../../services/user/user-service.service';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-product-card',
@@ -17,10 +19,23 @@ export class ProductCardComponent {
 
   addToCart() {
     console.log(this.product._id);
+    if (!this.product.stock) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'There is no enough quantity in the stock!',
+      });
+      return;
+    }
     this.userService.addCart(this.product._id).subscribe({
       next: (data) => { },
       error: (error) => console.error(error)
     });
+    Swal.fire({
+      icon: 'success',
+      title: 'Great!',
+      text: 'Product Added To Your Cart Successfully'
+    })
   }
 
 }
