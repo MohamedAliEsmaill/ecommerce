edbc5a98a8e189425d01c1e54fc15887a812e39c
Mohamed Hossny <mhossny2@outlook.com>
2024-04-28 12:18:22 +0300

    refactor(profile-info): Update profile-information component HTML structure and bindings

--- client/src/app/components/profile-information/profile-information.component.ts
+++ client/src/app/components/profile-information/profile-information.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { ProfileService } from '../../services/profile/profile.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-profile-information',
   standalone: true,
   imports: [],
+  providers: [ProfileService],
   templateUrl: './profile-information.component.html',
 })
 export class ProfileInformationComponent {
-
+  userInfo: any = {};
+  constructor(
+    public profileService: ProfileService,
+    public router: ActivatedRoute
+  ) {
+    this.router.url.subscribe((url) => {
+      console.log(url);
+    });
+    this.profileService.getProfile().subscribe({
+      next: (data) => {
+        console.log(data);
+        this.userInfo = data;
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
+  }
 }
