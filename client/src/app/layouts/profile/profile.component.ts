09eaa1cd76f70a769d9f4aa4c09de95b7beff38a
Mohamed Hossny <mhossny2@outlook.com>
2024-04-28 12:15:38 +0300

    feat(profile): import AccountComponent and ProfileService, and add user info and logout functionality

--- client/src/app/layouts/profile/profile.component.ts
+++ client/src/app/layouts/profile/profile.component.ts
@@ -1,14 +1,48 @@
+import { routes } from './../../app.routes';
 import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
-import { WishlistComponent } from '../../components/wishlist/wishlist.component';
+import {
+  ActivatedRoute,
+  NavigationEnd,
+  Router,
+  RouterModule,
+  RouterOutlet,
+} from '@angular/router';
+import { ProfileService } from '../../services/profile/profile.service';
+import { AccountComponent } from '../account/account.component';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [RouterModule, RouterOutlet, WishlistComponent],
+  imports: [RouterModule, RouterOutlet, AccountComponent],
+  providers: [ProfileService],
   templateUrl: './profile.component.html',
-  styles: ``
 })
 export class ProfileComponent {
+  userInfo: any = {};
+  showAccount: boolean = true;
+  constructor(public profileService: ProfileService, private router: Router) {
+    this.profileService.getProfile().subscribe({
+      next: (data) => {
+        console.log(data);
+        this.userInfo = data;
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
+    router.events.subscribe((val) => {
+      if (val instanceof NavigationEnd) {
+        if (val.url === '/profile') {
+          this.showAccount = true;
+        } else {
+          this.showAccount = false;
+        }
+      }
+    });
+  }
 
+  logout() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('role');
+  }
 }
