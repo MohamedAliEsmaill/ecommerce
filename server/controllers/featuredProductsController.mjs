dcde92615c925ed197f93f1f9a386c55b76835f6
MohamedAliEsmaill <mohamedali.official2020@gmail.com>
2024-04-29 11:00:48 +0300

    feat(controller): add featured products controller

---
+++ server/controllers/featuredProductsController.mjs
@@ -0,0 +1,11 @@
+import FeaturedProducts from "../models/FeaturedProducts.mjs";
+
+export const getFeaturedProducts = async (req, res) => {
+  try {
+    const featuredProducts = await FeaturedProducts.find({ featured: true });
+    res.json(featuredProducts);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error retrieving featured products" });
+  }
+};
